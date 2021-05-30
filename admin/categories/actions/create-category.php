43a116e4ef8bda251650ad5b89597ef9f4326d58
QueebSkeleton <queebskeleton@gmail.com>
2021-05-30 22:26:02 +0800

    Add functionality for adding categories

---
+++ admin/categories/actions/create-category.php
@@ -0,0 +1,40 @@
+<?php
+
+  // Only allow POST requests
+  // TODO: Add error-handling mechanism
+  if($_SERVER["REQUEST_METHOD"] != "POST") {
+    header("Status: 405 Method Not Allowed");
+    die("Only POST requests are allowed.");
+  }
+
+  // Get category details from request parameters
+  // TODO: add functionality for thumbnail
+  $name = $_POST["name"];
+  $parent_category_id = $_POST["parent_category_id"] != "0" ? $_POST["parent_category_id"] : NULL;
+  echo $parent_category_id;
+  $description = $_POST["description"];
+
+  // Parse config.ini file then get db credentials
+  $config = parse_ini_file("../../../config.ini");
+
+  // Get connection
+  $conn = mysqli_connect($config["db_server"], $config["db_user"], $config["db_password"], $config["db_name"]);
+
+  // Prepare insert statement
+  $insert_stmt = mysqli_prepare($conn, "INSERT INTO `product_category`(`name`, `description`, `parent_category_id`) VALUES (?, ?, ?)");
+
+  // Bind the category values
+  mysqli_stmt_bind_param($insert_stmt, "ssi", $name, $description, $parent_category_id);
+
+  // Execute insert statement
+  mysqli_stmt_execute($insert_stmt);
+
+  // Close all
+  mysqli_stmt_close($insert_stmt);
+  mysqli_close($conn);
+
+  // TODO: add error cases
+  // Redirect if success
+  header("Location: /admin/categories/panel.php");
+
+?>
