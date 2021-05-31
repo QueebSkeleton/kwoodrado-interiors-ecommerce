b7c2eb2e5400bd96a061af736f6c14ee426ddac7
QueebSkeleton <queebskeleton@gmail.com>
2021-05-31 14:17:26 +0800

    Change admin sidebar profile

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -1,9 +1,9 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
+  <a href="/admin/dashboard.php" class="brand-link">
     <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <span class="brand-text font-weight-light">Kwoodrado Int.</span>
   </a>
 
   <!-- Sidebar -->
@@ -11,10 +11,10 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <i class="fas fa-user-tie fa-2x text-light"></i>
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">Business Owner</a>
       </div>
     </div>
 
